909a16a88603287ac554a825e92e065a6706cf6a
MAeStROxr <maestroeyal@gmail.com>
2017-04-28 03:39:31 +0300

    Fixed memory sanitizer test case so it would trigger the memory sanitizer
    - Updated the error guide also

--- vera/tests/sanitizer/sanitizer_memory.c
+++ vera/tests/sanitizer/sanitizer_memory.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 int main(int argc, char** argv) 
 {
-	int res = -1;
 	int* arr = (int*)malloc(7*sizeof(int));
 	arr[ 2 ] = 0;  //  - OK - Write to uninitialized memory
-	if (arr[ 3 ]) // BOOM  - Read from uninitialized memory
+	if (arr[ argc ]) // BOOM  - Read from uninitialized memory
 	{
-		res = 1;
+		printf("xx\n");
 	}
 	free(arr);
-	return res;
+	return 0;
 }
